279c5001024f1fee91d92479fae6ac5b3516ca81
sadakchap <aliceprerna@gmail.com>
2020-02-11 16:13:33 +0530

    added some templates files

--- maa_shop/urls.py
+++ maa_shop/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', )
+    path('', home_view, name="home"),
     path("accounts/", include("accounts.urls")),
 ]
 
