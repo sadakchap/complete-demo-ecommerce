88def759b6b1e8404275de22798dced6faf1ce89
sadakchap <aliceprerna@gmail.com>
2020-02-11 15:49:52 +0530

    Created Custom User Model

---
+++ accounts/models.py
@@ -0,0 +1,80 @@
+from django.conf import settings
+from django.db import models
+from django.core.mail import send_mail
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError(_('Users Must have an Email Address'))
+        if not password:
+            raise ValueError(_('Users must have a password'))
+        
+        user_obj = self.model(
+            email=self.normalize_email(email)
+        )
+        user_obj.set_password(password)
+        user_obj.active = is_active
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.save(using=self._db)
+        return user_obj
+    
+    def create_staffuser(self, email, password=None):
+        return self.create_user(email, password, is_staff=True)
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password,
+            is_staff=True,
+            is_admin=True
+        )
+        return user
+
+
+class User(AbstractBaseUser):
+    email       = models.EmailField(_('email address'), unique=True)
+    first_name  = models.CharField(_("First name"), max_length=255, blank=True, null=True)
+    last_name   = models.CharField(_("Last name"), max_length=255, blank=True, null=True)
+    active      = models.BooleanField(_("active"), default=True)
+    staff       = models.BooleanField(_("staff"), default=False)
+    admin       = models.BooleanField(_("admin"), default=False)
+
+    #passowrd and last_login are included from AbstractBaseUser class
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    def get_full_name(self):
+        return self.first_name + self.last_name
+    
+    def get_short_name(self):
+        if self.first_name:
+            return self.first_name
+        return None
+    
+    def email_user(self, subject, message, **kwargs):
+        send_mail(subject, message, settings.EMAIL_HOST_USER,
+                  [self.email], **kwargs)
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_superuser(self):
+        return self.admin
+
+    @property
+    def is_staff(self):
+        return self.admin
+    
+    @property
+    def is_active(self):
+        return self.active
---
+++ maa_shop/settings.py
@@ -0,0 +1,103 @@
+import os
+from decouple import config, Csv
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = config('SECRET_KEY')
+
+DEBUG = config('DEBUG', default=True, cast=bool)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
+
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'crispy_forms',
+]
+
+AUTH_USER_MODEL = 'accounts.User'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'maa_shop.urls'
+
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'maa_shop.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Kolkata'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
