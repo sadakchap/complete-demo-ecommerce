958a24b3c9276914bb0df9dfd9a6c2d742e8fcee
sadakchap <aliceprerna@gmail.com>
2020-02-16 23:24:23 +0530

    confirm email sent done

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import login_required
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout, authenticate, get_user_model
 from .forms import UserLoginForm, UserRegisterForm
+from django.contrib import messages
 
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from .tokens import account_activation_token
+
+User = get_user_model()
 
 # Create your views here.
 def home_view(request):
@@ -40,7 +48,39 @@ def register_view(request):
             user = form.save(commit=False)
             user.set_password(password)
             user.save()
+            current_site = get_current_site(request)
+            subject = "Activate Your ---- Account"
+            message = render_to_string(
+                'accounts/account_activation_email.html',{
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                }
+            )
+            user.email_user(subject, message)
+            messages.info(request, "An activation link has been sent to your Email!")
             return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "accounts/register.html", {'form': form})
+
+
+def activate(request, uid, token):
+    try:
+        u_id = force_text(urlsafe_base64_decode(uid))
+        user = User.objects.get(pk=u_id)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    
+    if user is not None and account_activation_token.check_token(user, token):
+        user.active = True
+        user.profile.confirmed_email = True
+        user.profile.set_confirmed_date()
+        user.save()
+        login(request, user)
+        messages.success(request, "we have confirmed your email!")
+        return redirect("/")
+    else:
+        messages.error(request, "Account Activation Failed!")
+        return render(request, "accounts/activation_invalid.html")
