bd09833786ecda08d0262c7ccca926226c8248f4
sadakchap <aliceprerna@gmail.com>
2020-03-13 20:37:43 +0530

    fixed some errors

--- cart/cart.py
+++ cart/cart.py
@@ -7,7 +7,7 @@ class Cart(object):
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            cart = self.session[settings.CART_SESSION_ID]
+            cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def add(self, product, quantity=1, update_quantity=False):
@@ -47,10 +47,10 @@ class Cart(object):
             yield item
     
     def __len__(self):
-        return sum(item['quantity'] for item in self.cart.value())
+        return sum(item['quantity'] for item in self.cart.values())
     
     def get_total_price(self):
-        return sum(item['quantity'] * Decimal(item['price']) for item in self.cart.value())
+        return sum(item['quantity'] * Decimal(item['price']) for item in self.cart.values())
     
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
