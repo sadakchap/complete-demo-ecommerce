5771de142561302ab9dcbe8f32f2f08e29c0c7a6
sadakchap <aliceprerna@gmail.com>
2020-03-26 23:37:59 +0530

    search bar

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+
 from .models import Category, Product
 from cart.forms import CartAddProductForm
 
@@ -10,6 +12,16 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+    
+    query = request.GET.get('q')
+    if query:
+        query_set = query.split()
+        for query in query_set:
+            products = products.filter(
+                Q(name__icontains=query) | 
+                Q(desc__icontains=query) | 
+                Q(category__name__icontains=query)
+            ).distinct()
     
     return render(request, 'products/product_list.html', {
         'products': products,
